04422322b09f71db084319805fe4d82c31d9ca8a
Anmol Arora <heymailme04@gmail.com>
2024-03-05 13:08:18 +0530

    Add files via upload

---
+++ frontEndQala/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar.component";
+import UserAuthForm from "./pages/userAuthForm.page";
+import HomePage from "./pages/home.page";
+import Blog from "./pages/blog.page";
+import Explore from "./pages/explore.page";
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+        <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
