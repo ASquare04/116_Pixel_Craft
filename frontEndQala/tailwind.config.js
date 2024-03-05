8cb0cd5655a64bcf7949c406e4c4f028759012b2
Anmol Arora <heymailme04@gmail.com>
2024-03-05 13:05:05 +0530

    Add files via upload

---
+++ frontEndQala/tailwind.config.js
@@ -0,0 +1,31 @@
+    /** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+      extend: {
+        colors:{
+            'seagreen':'#d1d65c',
+            'seagreen-100': '#e5e9b7',
+            'black-100':'#080808',
+            'black-400':'#101010'
+        }
+      },
+    },
+    plugins: []
+  }
