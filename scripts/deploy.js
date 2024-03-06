b710318b16e2494583c99075de38d02f65af6ebb
Anmol Arora <heymailme04@gmail.com>
2024-03-07 04:42:09 +0530

    Add files via upload

---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+
+const hre = require("hardhat");
+
+async function main() {
+
+  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+    value: lockedAmount,
+  });
+
+  await lock.waitForDeployment();
+
+  console.log(
+    `Lock with ${ethers.formatEther(
+      lockedAmount
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
