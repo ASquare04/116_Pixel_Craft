ae8b297ba93d48369baba25a1b26e4a6a0cf3f9c
Anmol Arora <heymailme04@gmail.com>
2024-03-07 04:40:12 +0530

    Update App.jsx

--- frontEndQala/src/App.jsx
+++ frontEndQala/src/App.jsx
@@ -4,15 +4,20 @@ import UserAuthForm from "./pages/userAuthForm.page";
 import HomePage from "./pages/home.page";
 import Blog from "./pages/blog.page";
 import Explore from "./pages/explore.page";
+import ExploreDetails from './pages/explore.details.page';
+import BlogDetails from "./pages/blog.details.page";
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navbar />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:id/:featuredImg/:title/:content/:relatedImg1/:relatedImg2" element={<BlogDetails />}/>
         <Route path="/explore" element={<Explore />} />
-{/*         <Route path="signin" element={<UserAuthForm type="sign-in" />} />
-        <Route path="signup" element={<UserAuthForm type="sign-up" />} /> */}
+        <Route path="/explore/:id/:heading/" element={<ExploreDetails />} />
+        <Route path="/signin" element={<UserAuthForm type="sign-in" />} />
+        <Route path="/signup" element={<UserAuthForm type="sign-up" />} />
       </Route>
     </Routes>
   );
