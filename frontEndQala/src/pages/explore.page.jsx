d9698e8ec689a19e2e1ad2b4f03f6e6e7aede070
Anmol Arora <heymailme04@gmail.com>
2024-03-07 04:39:31 +0530

    Add files via upload

---
+++ frontEndQala/src/pages/explore.page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Gallery from "../components/gallery.component";
+
+const Explore = () => {
+  return (
+    <div className="bg-black h-1/2">
+      <Gallery />
+    </div>
+  );
+};
+
+export default Explore;
